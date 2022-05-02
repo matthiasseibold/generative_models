e64d8b491489f41291149e950f106c129dae0172
Matthias Seibold <matthias.seibold@tum.de>
2022-05-02 12:16:34 +0200

    cleaned up repo

--- utils/run_batch.bat
+++
@@ -1,7 +0,0 @@
-REM Run the scripts from batch file because of memory leak in TF implementation of SpecAugment
-
-python AugmentSpecAugment.py
-python AugmentSpecAugment1.py
-python AugmentSpecAugment2.py
-python AugmentSpecAugment3.py
-python AugmentSpecAugment4.py
